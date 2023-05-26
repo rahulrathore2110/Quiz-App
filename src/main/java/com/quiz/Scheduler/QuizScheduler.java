d06e43da349cdd7545225c70711cdbb1ee0f6a92
Rahul Rathore <rahulg27727@gmail.com>
2023-05-27 00:00:31 +0530

    Final

--- src/main/java/com/quiz/Scheduler/QuizScheduler.java
+++ src/main/java/com/quiz/Scheduler/QuizScheduler.java
@@ -5,6 +5,7 @@ import com.quiz.Model.Status;
 import com.quiz.Repository.QuizDao;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -20,6 +21,7 @@ public class QuizScheduler {
     }
 
     @Scheduled(cron = "0 */1 * * * ?")
+    @Transactional
     public void quizStatusUpdate() {
 
 
