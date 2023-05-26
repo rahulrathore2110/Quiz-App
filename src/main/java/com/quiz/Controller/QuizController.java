42fa80180306e290fda11f198c9c3a9b274800b4
Rahul Rathore <rahulg27727@gmail.com>
2023-05-26 23:24:14 +0530

    Final

--- src/main/java/com/quiz/Controller/QuizController.java
+++ src/main/java/com/quiz/Controller/QuizController.java
@@ -7,13 +7,13 @@ import com.quiz.Service.QuizService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/quizzes")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class QuizController {
 
 
